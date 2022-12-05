91a69d0412b4bc2f2c47891e82aeb397a2b14d2d
Atul Patidar <48468580+AtulPatidar1709@users.noreply.github.com>
2022-12-05 19:22:30 +0000

    Minor Changes in QA

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
 var readlineSync = require("readline-sync");
 var score = 0;
 var userName = readlineSync.question("What is your Name? ");
-console.log("Hi " + userName + "Do You Know Atul? ");
+console.log("Hi " + userName + " Do You Know Atul? ");
 
-function play(question,answer){
+function play(question, answer) {
   var userAnswer = readlineSync.question(question);
-  
-  if (userAnswer === answer){
+
+  if (userAnswer === answer) {
     console.log("Yes you are wright");
     score = score + 1;
   } else {
     console.log("You are Wrong Try Again");
     score = score;
   }
-    console.log("Your Score is " + score);
-    console.log("____________")
-  
-  if (score === 3){
-  console.log("Level One Completed");
-  console.log("__________")
-}
-  if (score === 5){
-   console.log("You Know Me very well");
-  console.log("__________")
- }
- if(score === 1){
+  console.log("Your Score is " + score);
+  console.log("____________")
+
+  if (score === 4) {
+    console.log("Congratulations You know me very well");
+    console.log("__________")
+  }
+  if (score === 5) {
+    console.log("You Know Me very well");
+    console.log("__________")
+  }
+  if (score === 1) {
     console.log("You need to Know me");
- }
+  }
 }
 
-play ("What is My Name","Atul" );
+play("What is My Name ", "Atul");
 
-play ("What is my Age","22" );
+play("What is my Age ", "22");
 
-play ("What is my City","Ashta" );
+play("What is my City ", "Ashta");
 
-play ("What is my Love","Maa" );
+play("who is my Love ", "Maa");
 
-play ("I Lives in ","Ashta" );
+play("I am in love with", "Chai");
